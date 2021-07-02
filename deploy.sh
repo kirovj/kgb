34af842a69ae41e83d21943c6000b83cfd86ce1d
J-wyt <546108612@qq.com>
2021-07-02 14:30:05 +0800

    add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ps auwx | grep "kgb" | grep -v "grep" | grep "wyt" | awk '{print $2}' | xargs kill -9
+
+echo "git pull code..."
+git pull
+
+cd bin
+
+rm kgb
+
+echo "start go build..."
+
+go build kgb.go
+
+nohup ./kgb > /dev/null 2>&1 &
+echo "run kgb success"
--- start.sh
+++
@@ -1,5 +0,0 @@
-ps auwx | grep "kgb" | grep -v "grep" | grep "wyt" | awk '{print $2}' | xargs kill -9
-git pull
-cd bin
-go build kgb.go
-nohup ./kgb > /dev/null 2>&1 &
